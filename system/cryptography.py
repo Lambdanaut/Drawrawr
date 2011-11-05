cb0dcc5d7f292ad5e9f197c87261ea6c8a9173df
Jonny Lettman <john@septmedia.com>
2011-11-05 19:56:33 -0400

    Main: added options & Crypto: syntax change

--- system/cryptography.py
+++ system/cryptography.py
@@ -2,11 +2,8 @@ from math import floor
 from hashlib import sha512
 
 def saltPassword(password): 
-  if len(password) > 4:
-    password = "SDDG$##@Hfa" + password.upper() + "3r#$^&2andgjngds"
-  else:
-    password = "#%!DD" + password + "$#&#N%JN323##)(#@#fmmhsppf{s}{[|11^^^43n4jfw@"
-  
+  if len(password) > 4: password = "SDDG$##@Hfa" + password.upper() + "3r#$^&2andgjngds"
+  else: password = "#%!DD" + password + "$#&#N%JN323##)(#@#fmmhsppf{s}{[|11^^^43n4jfw@-"
   return password
 
 def encryptPassword(password, salt = False):
