fec9c92c860c3a28f603fe7b29a9c2886b6095aa
Josh Thomas <personettelabs@gmail.com>
2012-02-28 18:20:40 -0500

    Trying to get heroku running

--- system/database.py
+++ system/database.py
@@ -11,7 +11,7 @@ class Database:
     self.con = pymongo.Connection(host, port)
     self.db  = self.con.DR
     if username and password:
-      db.authenticate(username, password)
+      self.db.authenticate(username, password)
 
   def userPassCheck(self,username,password):
     user = self.db.users.find_one({'lowername' : username.lower(),'password' : password})
